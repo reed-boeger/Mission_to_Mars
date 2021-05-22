b05ac8c73d75712b6db61319eeae051c0507c27c
reed-boeger <boeger.reed@gmail.com>
2021-05-22 00:13:31 -0400

    finished deliverable 1 and 2

--- scraping.py
+++ scraping.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import datetime as dt
 
+
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
@@ -19,7 +20,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemisphere_image_urls": hemispheres(browser)
     }
 
     # Stop webdriver and return data
@@ -96,7 +98,27 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
+
+def hemispheres(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    hemisphere_image_urls=[]
+    for i in range (4):
+        hemispheres={}
+        images = browser.find_by_tag('h3')
+        images[i].click()
+        html = browser.html
+        hemi_soup = soup(html, 'html.parser')
+        partial = hemi_soup.find("img", class_="wide-image")["src"]
+        img_title = hemi_soup.find("h2",class_="title").text
+        img_url = url + partial
+        hemispheres={"img_url":img_url, "title":img_title}
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+    return hemisphere_image_urls
+
 if __name__ == "__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
+
